2ccd15ee35af716effdfd9cd9e108640dcfc106c
kush-xerovit <kush@xerovit.io>
2021-04-21 10:43:54 +0630

    fixed console error

--- src/app/shared/components/dynamic-field/dynamic-field.module.ts
+++ src/app/shared/components/dynamic-field/dynamic-field.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
@@ -62,5 +62,6 @@ const modules = [
     QuillModule.forRoot(),
   ],
   exports: [...components, ...directive, ...modules],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class DynamicFieldModule {}
